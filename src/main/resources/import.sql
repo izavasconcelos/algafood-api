33e18fc2bd4e8bd6c4a565c8a7135a18a6a7bd68
izavasconcelos <vasconcelos.izas@gmail.com>
2021-06-10 09:45:45 -0300

    create Repository of Restaurante

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -3,3 +3,6 @@ insert into tab_cozinhas (nome) values ('Indiana');
 
 insert into tab_restaurantes (nome, taxa_frete)  values ('Outback', 5.00);
 insert into tab_restaurantes (nome, taxa_frete) values ('Petisqueira', 10.00);
+insert into tab_restaurantes (nome, taxa_frete) values ('Thai Gourmet', 10);
+insert into tab_restaurantes (nome, taxa_frete) values ('Thai Delivery', 9.50);
+insert into tab_restaurantes (nome, taxa_frete) values ('Tuk Tuk Comida Indiana', 15);
