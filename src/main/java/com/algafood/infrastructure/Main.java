aaf1bb1b0a94544621353a6d2427f8b6d02da1c6
izavasconcelos <vasconcelos.izas@gmail.com>
2021-06-10 09:41:30 -0300

    create Repository of Restaurante

--- src/main/java/com/algafood/infrastructure/Main.java
+++ src/main/java/com/algafood/infrastructure/Main.java
@@ -2,11 +2,14 @@ package com.algafood.infrastructure;
 
 import com.algafood.AlgafoodApplication;
 import com.algafood.domain.entity.Cozinha;
+import com.algafood.domain.entity.Restaurante;
 import com.algafood.domain.repository.CozinhaRepository;
+import com.algafood.domain.repository.RestauranteRepository;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ApplicationContext;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 public class Main {
@@ -17,6 +20,7 @@ public class Main {
             .run(args);
 
     CozinhaRepository repository = context.getBean(CozinhaRepository.class);
+    RestauranteRepository restauranteRepository = context.getBean(RestauranteRepository.class);
     List<Cozinha> cozinhaList = repository.findAll();
     cozinhaList.forEach(System.out::println);
 
@@ -29,5 +33,21 @@ public class Main {
     System.out.println("\n**"+ repository.getById(1L));
 
     repository.delete(cozinhaAdd);
+
+    // *****************************************
+
+    List<Restaurante> restauranteList = restauranteRepository.findAll();
+    restauranteList.forEach(System.out::println);
+
+    Restaurante restaurante = new Restaurante();
+    restaurante.setNome("Brasil");
+    restaurante.setTaxaFrete(BigDecimal.valueOf(2.50));
+
+    Restaurante restauranteAdd = restauranteRepository.save(restaurante);
+    System.out.println(restauranteAdd);
+
+    System.out.println("\n**"+ restauranteRepository.getById(1L));
+
+    //restauranteRepository.delete(restauranteAdd);
   }
 }
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,5 @@
 insert into tab_cozinhas (nome)  values ('Tailandesa');
 insert into tab_cozinhas (nome) values ('Indiana');
+
+insert into tab_restaurantes (nome, taxa_frete)  values ('Outback', 5.00);
+insert into tab_restaurantes (nome, taxa_frete) values ('Petisqueira', 10.00);
