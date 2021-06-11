bb7f285ccb223a31d256564ed7df49b95d09f809
izavasconcelos <vasconcelos.izas@gmail.com>
2021-06-11 18:20:56 -0300

    update import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -6,3 +6,10 @@ insert into tab_restaurantes (nome, taxa_frete, cozinha_id) values ('Petisqueira
 insert into tab_restaurantes (nome, taxa_frete, cozinha_id) values ('Thai Gourmet', 10, 2);
 insert into tab_restaurantes (nome, taxa_frete, cozinha_id) values ('Thai Delivery', 9.50, 1);
 insert into tab_restaurantes (nome, taxa_frete, cozinha_id) values ('Tuk Tuk Comida Indiana', 15, 2);
+
+insert into tab_pagamentos (descricao, restaurante_id) values ('Cartão de crédito', 2);
+insert into tab_pagamentos (descricao, restaurante_id) values ('Cartão de débito', 3);
+insert into tab_pagamentos (descricao, restaurante_id) values ('Dinheiro', 1);
+
+insert into tab_permissoes (nome, descricao) values ('CONSULTAR_COZINHAS', 'Permite consultar cozinhas');
+insert into tab_permissoes (nome, descricao) values ('EDITAR_COZINHAS', 'Permite editar cozinhas');
