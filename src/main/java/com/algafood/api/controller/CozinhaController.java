fccbd35fc11d67aa5eb55e1ea6664661b13b3471
izavasconcelos <vasconcelos.izas@gmail.com>
2021-06-16 16:36:49 -0300

    create findById

--- src/main/java/com/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/algafood/api/controller/CozinhaController.java
@@ -4,6 +4,7 @@ import com.algafood.domain.entity.Cozinha;
 import com.algafood.domain.repository.CozinhaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/cozinhas")
+//@RequestMapping(value = "/cozinhas", produces = MediaType.APPLICATION_JSON_VALUE)
 public class CozinhaController {
 
     @Autowired
@@ -19,5 +21,10 @@ public class CozinhaController {
     @GetMapping
     public List<Cozinha> findAll() {
         return cozinhaRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Cozinha findById(@PathVariable Long id) {
+        return cozinhaRepository.getById(id);
     }
 }
--- src/main/java/com/algafood/api/controller/EstadoController.java
+++ src/main/java/com/algafood/api/controller/EstadoController.java
@@ -4,6 +4,7 @@ import com.algafood.domain.entity.Estado;
 import com.algafood.domain.repository.EstadoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +20,10 @@ public class EstadoController {
     @GetMapping
     public List<Estado> findAll() {
         return estadoRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Estado findById(@PathVariable Long id) {
+        return estadoRepository.getById(id);
     }
 }
