b5e8575cae0130d294d74cc0d61b20ac8501e89a
izavasconcelos <vasconcelos.izas@gmail.com>
2021-06-16 16:11:21 -0300

    create EstadoController

---
+++ src/main/java/com/algafood/api/controller/EstadoController.java
@@ -0,0 +1,23 @@
+package com.algafood.api.controller;
+
+import com.algafood.domain.entity.Estado;
+import com.algafood.domain.repository.EstadoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/estados")
+public class EstadoController {
+
+    @Autowired
+    private EstadoRepository estadoRepository;
+
+    @GetMapping
+    public List<Estado> findAll() {
+        return estadoRepository.findAll();
+    }
+}
