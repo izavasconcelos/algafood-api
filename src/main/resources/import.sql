c3c8c5c26d03daeb7fc1ffafd1101c768e2c56f8
izavasconcelos <vasconcelos.izas@gmail.com>
2021-06-16 16:10:53 -0300

    update import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,6 +1,9 @@
 insert into tab_cozinhas (nome)  values ('Tailandesa');
 insert into tab_cozinhas (nome) values ('Indiana');
 
+insert into tab_estados (nome) values ('RS');
+insert into tab_estados (nome) values ('SP');
+
 insert into tab_restaurantes (nome, taxa_frete, cozinha_id)  values ('Outback', 5.00, 1);
 insert into tab_restaurantes (nome, taxa_frete, cozinha_id) values ('Petisqueira', 10.00, 1);
 insert into tab_restaurantes (nome, taxa_frete, cozinha_id) values ('Thai Gourmet', 10, 2);
