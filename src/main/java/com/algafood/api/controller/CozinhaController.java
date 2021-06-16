78cb6464e4f701d5e937bb04cf2efcb3dade5908
izavasconcelos <vasconcelos.izas@gmail.com>
2021-06-16 16:11:12 -0300

    create CozinhaController

---
+++ src/main/java/com/algafood/api/controller/CozinhaController.java
@@ -0,0 +1,23 @@
+package com.algafood.api.controller;
+
+import com.algafood.domain.entity.Cozinha;
+import com.algafood.domain.repository.CozinhaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/cozinhas")
+public class CozinhaController {
+
+    @Autowired
+    private CozinhaRepository cozinhaRepository;
+
+    @GetMapping
+    public List<Cozinha> findAll() {
+        return cozinhaRepository.findAll();
+    }
+}
