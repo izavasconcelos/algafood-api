d1cd0929b4feffcee3ae3ee57e4308dd1597381a
izavasconcelos <vasconcelos.izas@gmail.com>
2021-06-16 18:11:51 -0300

    create save

--- src/main/java/com/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/algafood/api/controller/CozinhaController.java
@@ -3,28 +3,37 @@ package com.algafood.api.controller;
 import com.algafood.domain.entity.Cozinha;
 import com.algafood.domain.repository.CozinhaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/cozinhas")
-//@RequestMapping(value = "/cozinhas", produces = MediaType.APPLICATION_JSON_VALUE)
 public class CozinhaController {
 
     @Autowired
     private CozinhaRepository cozinhaRepository;
 
     @GetMapping
-    public List<Cozinha> findAll() {
-        return cozinhaRepository.findAll();
+    public ResponseEntity<List<Cozinha>> findAll() {
+        return ResponseEntity.ok(cozinhaRepository.findAll());
     }
 
     @GetMapping("/{id}")
-    public Cozinha findById(@PathVariable Long id) {
-        return cozinhaRepository.getById(id);
+    public ResponseEntity<Cozinha> findById(@PathVariable Long id) {
+
+        Cozinha cozinha = cozinhaRepository.getById(id);
+        if (cozinha != null) {
+            return ResponseEntity.ok(cozinha);
+        }
+
+        return ResponseEntity.notFound().build();
     }
+
+    @PostMapping
+	public ResponseEntity<Cozinha> save(@RequestBody Cozinha cozinha) {
+    	return ResponseEntity.status(HttpStatus.CREATED).body(cozinhaRepository.save(cozinha));
+	}
 }
