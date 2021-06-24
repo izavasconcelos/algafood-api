864e848ecc310ba829e63c6000c74fe8a8e360c6
izavasconcelos <vasconcelos.izas@gmail.com>
2021-06-24 17:03:22 -0300

    created Produto

---
+++ src/main/java/com/algafood/domain/entity/Produto.java
@@ -0,0 +1,36 @@
+package com.algafood.domain.entity;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Table(name = "tab_produtos")
+public class Produto {
+
+	@EqualsAndHashCode.Include
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false)
+	private String name;
+
+	@Column(nullable = false)
+	private String description;
+
+	@Column(nullable = false)
+	private BigDecimal price;
+
+	@Column(nullable = false)
+	private Boolean active;
+
+	@ManyToOne
+	@JoinColumn(nullable = false)
+	private Restaurante restaurante;
+
+}
